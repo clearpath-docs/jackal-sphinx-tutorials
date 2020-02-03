f966e9c0f892bb69a4ac47d4ea0b4b841bbe9d58
Chris I-B <civerachb@clearpathrobotics.com>
2020-02-03 15:29:25 -0500

    Add latex configuration

--- doc/conf.py
+++ doc/conf.py
@@ -56,3 +56,35 @@ html_logo = 'clearpathlogo.png'
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 html_favicon = 'favicon.ico'
+
+# -- Options for LaTeX output ------------------------------------------------
+latex_engine = "xelatex"
+latex_docclass = {
+   'howto': 'clearpath-latex/clearpath-manual',
+   'manual': 'clearpath-latex/clearpath-manual',
+}
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+    (master_doc, 'JackalTutorials.tex', 'Jackal Tuturials',
+     'Dave Niewinski', 'manual'),
+]
